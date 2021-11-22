172d489d37a5879fd2e9e0b09040cdc948879769
Agoitz <aezkerrabe21dw@ikzubirimanteo.com>
2021-11-22 13:30:07 +0100

    Cambio simple tabla de multiplos de 2

--- src/Prueba.java
+++ src/Prueba.java
@@ -3,6 +3,10 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!");
+		for(int i = 0; i <= 12; i++)
+        {
+            System.out.print("2 * "+ i + " = " + 2 * i + "\n");
+        }
 	}
 
 }
